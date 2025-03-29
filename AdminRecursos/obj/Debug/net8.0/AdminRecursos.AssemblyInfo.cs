0f7d4cb2f0b8b6e4b34191c2cdd2d39b9154657b
ACEU901 <andrea.espinozau@correoaiep.cl>
2025-03-29 10:48:22 -0300

    se agregan clases temperatura

--- AdminRecursos/obj/Debug/net8.0/AdminRecursos.AssemblyInfo.cs
+++ AdminRecursos/obj/Debug/net8.0/AdminRecursos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminRecursos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8157f5df7abb293309dd1dd24bc47d8d20d95daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminRecursos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminRecursos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
